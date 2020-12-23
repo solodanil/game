db3332b8bd931a0debb6cb80cb13c5ea79d2044c
Daniil <solo.danil@mail.ru>
2020-12-23 19:08:46 +0300

    image_load and image direct

---
+++ tools.py
@@ -0,0 +1,25 @@
+import os
+
+import pygame
+import pygame.examples.eventlist
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('images', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+if __name__ == '__main__':
+    pygame.examples.eventlist.main()
